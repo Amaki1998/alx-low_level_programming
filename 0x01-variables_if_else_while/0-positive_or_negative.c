d35d0030af4fdf10edb1e9c826a257626facfb41
Amanuel <amanuelakirso@gmail.com>
2022-06-03 09:06:56 -0700

    positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,17 +9,17 @@ scanf("%d", &n);
 if (n > 0)
 {
 printf("%d ", n);
-printf("is positive\n");
+printf("is positive\n", n);
 }
 else if (n < 0)
 {
 printf("%d ", n);
-printf("is negative\n");
+printf("is negative\n", n);
 }
 else
 {
 printf("%d ", n);
-printf("is zero\n");
+printf("is zero\n", n);
 }
 return (0);
 }
