22d5dbd2ea0a8fa1ad38b57cf652575302ada4c3
Amanuel <amanuelakirso@gmail.com>
2022-06-03 13:26:42 -0700

    adsf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ if (last >  5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
-else if (last_digit == 0)
+else if (last == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last);
 }
