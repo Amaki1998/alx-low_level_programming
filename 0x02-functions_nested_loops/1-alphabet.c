a93152c1e6c482824f3d9b2e2f6781dae44825ec
Amanuel <amanuelakirso@gmail.com>
2022-06-07 03:59:52 -0700

    small alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet- prints small letter alphabets
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+void print_alphabet(void);
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
