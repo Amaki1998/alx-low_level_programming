beb6cea4076032867cc112158831a08c8b339386
Amanuel <amanuelakirso@gmail.com>
2022-06-07 09:55:12 -0700

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,12 @@ int print_last_digit(int x)
 int last_dig = x % 10;
 if (last_dig > 0)
 {
+_putchar(last_dig + '0');
 return (last_dig);
 }
 else
 {
+_putchar(-last_dig + '0')
 return (-lasst_dig);
 }
 }
