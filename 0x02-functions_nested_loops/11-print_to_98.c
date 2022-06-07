45bc3c7f7183981a2ed7a03b927ab3bbdfe32fcd
Amanuel <amanuelakirso@gmail.com>
2022-06-07 15:48:26 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_to_98 - function prints all natural no from nto 98
+ *
+ * @n: variable
+ *
+ */
+void print_to_98(int n)
+{
+	int last = 98;
+
+	if (n > last)
+	{
+		while (n >= last)
+		{
+			if (n != last)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= last)
+		{
+			if (n != last)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+				_putchar(' ';)
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			n++;
+		}
+	}
+	_putchar('\n');
+}
