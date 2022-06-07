975b6c53b2fc5671687768775459a9a36d67e069
Amanuel <amanuelakirso@gmail.com>
2022-06-07 05:13:28 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,12 @@ int main(void)
 print_alphabet();
 return (0);
 }
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
+{
 _putchar(ch);
 _putchar('\n');
 }
-
+}
