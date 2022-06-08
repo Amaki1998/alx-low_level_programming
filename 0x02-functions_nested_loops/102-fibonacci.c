dd9fb4b41d8ab8d1cb26dd53c95e2e5735afa96d
Amanuel <amanuelakirso@gmail.com>
2022-06-08 01:55:11 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - core function prints first fibonacci numbers
+ *
+ * Return: exit program
+ */
+int main(void)
+{
+	long int x = 1, y = 1, z = 1, a;
+
+	printf("%ld", y);
+	while (x < 50)
+	{
+		a = y + z;
+		printf(", %ld", a);
+		y = z;
+		z = a;
+	}
+	x++;
+	putchar('\n');
+
+	return (0);
+}
