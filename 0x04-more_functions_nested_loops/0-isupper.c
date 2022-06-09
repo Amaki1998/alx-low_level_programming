0f131cb44cb9a2084823609366745cee3dcc7a46
Amanuel <amanuelakirso@gmail.com>
2022-06-09 07:04:17 -0700

    upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _isupper - function that checks for uppercase character
- *
- * @c: character
- *
- * Return: return 1 for upper case else 0 
- */
+* _isupper - function that checks for uppercase character
+*
+* @c: character
+*
+* Return: return 1 for uppercase else return 0
+*/
+
 int _isupper(int c)
 {
 	if (c >= 97 && c <= 122)
