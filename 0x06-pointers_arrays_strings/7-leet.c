caa8ef23cef82f203eec848df0420d07641959d6
Amanuel <amanuelakirso@gmail.com>
2022-06-16 07:04:06 -0700

    encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - function encodes a string in 1337
+ * @s: string
+ * Return: return string
+ */
+char *leet(char *s)
+{
+	int i = 0, j = 0, k = 0;
+	int let[5] = {'a', 'e', 'o', 't', 'l'};
+	int rep[5] = {4, 3, 0, 7, 1};
+
+	while (*(s + i))
+	{
+		while (j < 5)
+		{
+			k = let[j] - ' ';
+			if (s[i] == let[j] || s[i] == k)
+			{
+				s[i] = rep[j] + '0';
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
