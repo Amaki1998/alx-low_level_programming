2b50d07e4fca1ec9d02defd50e1897fdb7f2e586
Amanuel <amanuelakirso@gmail.com>
2022-06-16 14:25:17 -0700

    prints integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,10 +12,9 @@ void print_number(int n)
 		_putchar('-');
 		x = -x;
 	}
-	else
+	if (x / 10)
 	{
-		if (x / 10)
-			print_number(x / 10);
-		_putchar(x % 10 + '0');
+		print_number(x / 10);
 	}
+	_putchar(x % 10 + '0');
 }
