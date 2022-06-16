9471594e0095d2f09a0ece685ff57e43915e4750
Amanuel <amanuelakirso@gmail.com>
2022-06-16 02:23:10 -0700

    compare two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s2 == *s1)
+	while (*s1 == *s2)
 	{
-		if (*s2 == 0)
+		if (*s1 == 0)
 		{
 			return (0);
 		}
 		s1++;
 		s2++;
 	}
-	return (*s2 - *s1);
+	return (*s1 - *s2);
 }
