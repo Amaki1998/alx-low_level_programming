289a7ad5d5b7f4e4da3883b68673e12e1c48f349
Amanuel <amanuelakirso@gmail.com>
2022-06-22 15:17:48 -0700

    returns palindrome string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * is_palindrome - function that find a palindrome string
+ * @s: string
+ * Return: returns 1 if is palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int x;
+
+	x = 0;
+
+	if (*s == x)
+		return (0);
+	return (1);
+}
