050babbef319e832a61a9991d20dfbc9ddcc63d2
Amanuel <amanuelakirso@gmail.com>
2022-07-05 23:42:19 -0700

    executes the function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that execute a function pointer on each
+ * element of an array
+ * @array: array
+ * @size: size
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x = 0;
+
+	while (x < size)
+	{
+		action(array[x]);
+		x++;
+	}
+}
