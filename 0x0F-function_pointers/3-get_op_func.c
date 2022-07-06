51447c37d5cae8a1df65887fc97daf0fce4168bd
Amanuel <amanuelakirso@gmail.com>
2022-07-06 11:29:15 -0700

    performs simple operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * get_op_func - select the correct operation fxn
  * @s: operator
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * main - main function
  * @argc: argument count
@@ -9,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int num1, num2;
 	int (*k)(int, int);
 
 	if (argc != 4)
@@ -17,8 +15,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	k = get_op_func(argv[2]);
 	if (k == NULL)
 	{
@@ -26,7 +24,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	printf("%d\n", k(a, b));
+	printf("%d\n", k(num1, num2));
 
 	return (0);
 }
