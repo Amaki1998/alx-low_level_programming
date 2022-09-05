6606dd6697fdf0ef9a2d175320a1efde715a4975
Amanuel <amanuelakirso@gmail.com>
2022-09-05 16:19:59 -0700

    dynamic libraries

---
+++ 0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - core function
+ *
+ * Return: 0 return to exit
+ */
+
+int main(void)
+{
+_putchar(_);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+_putchar('\n');
+return (0);
+}
+
+
